ebf2770145d9fba0f0cfaaf21b84385b52ec17c1
dizo <dizo@list.ru>
2019-05-28 20:06:56 +0300

    2.4

--- lesson2-3.php
+++ lesson2-3.php
@@ -9,16 +9,15 @@ $thedate = date(N);
 //echo $arr[$thedate - 1];
 function dayB($arr, $thedate){
   switch ($thedate) {
-    case 1:$arr[$thedate - 1] = "<br>Понедельник</br>";break;
-    case 2:$arr[$thedate - 1] = "<br>Вторник</br>";break;
-    case 3:$arr[$thedate - 1] = "<br>Среда</br>";break;
-    case 4:$arr[$thedate - 1] = "<br>Четверг</br>";break;
-    case 5:$arr[$thedate - 1] = "<br>Пятница</br>";break;
-    case 6:$arr[$thedate - 1] = "<br>Суббота</br>";break;
-    case 0:$arr[$thedate - 1] = "<br>Воскресенье</br>";break;
+    case 1:$arr[$thedate - 1] = "<b>Понедельник</b>";break;
+    case 2:$arr[$thedate - 1] = "<b>Вторник</b>";break;
+    case 3:$arr[$thedate - 1] = "<b>Среда</b>";break;
+    case 4:$arr[$thedate - 1] = "<b>Четверг</b>";break;
+    case 5:$arr[$thedate - 1] = "<b>Пятница</b>";break;
+    case 6:$arr[$thedate - 1] = "<b>Суббота</b>";break;
+    case 0:$arr[$thedate - 1] = "<b>Воскресенье</b>";break;
   }
   var_dump($arr);
-  return;
 }
 dayB($arr, $thedate);
 
